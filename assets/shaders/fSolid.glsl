fee1c1f985da03814375cf320a37717f71085114
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-10-10 13:11:52 +0100

    turned off debug spin by default, removed rotation from model

--- assets/shaders/fSolid.glsl
+++ assets/shaders/fSolid.glsl
@@ -9,7 +9,7 @@ out vec4 FragColor;
 void main() { // expect 4 (FLOAT)
     float dist = length(vCamPos - vWorldPos);
     float darken = clamp(1.0 - dist * 0.05, 0.1, 1.0);
-    vec3 base = vec3(0.9, 0.5, 0.7);
+    vec3 base = vec3(0.0, 1.0, 0.1);
     float ambient_strength = 0.2;
     vec3 light_color = vec3(1.0, 1.0, 1.0);
 
