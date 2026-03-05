a534328aae74923fe91be5e958ee6f4cf47ace87
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-03-05 01:25:36 +0000

    Modified to include websockets server

--- internal/server/PerfectServer.hpp
+++ internal/server/PerfectServer.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
+#include <hv/WebSocketServer.h>
+#include <thread>
+
 namespace server
 {
     class PerfectServer {
     public:
-        PerfectServer() {
+        PerfectServer(int port);
+        ~PerfectServer();
 
-        }
+        void setPort(int p);
+
+        void start();
+        void stop();
 
         // WS server goes here. ECS system goes
     private:
-        void stop() {
-
-        }
+        int port;
+        std::jthread ws_thread;
+        hv::WebSocketService ws;
+        hv::WebSocketServer ws_server;
+        void async_ws_run(std::stop_token st);
+        bool running = false;
     };
 }
--- src/server/PerfectServer.cpp
+++ src/server/PerfectServer.cpp
@@ -1,15 +1,50 @@
+#include "server/PerfectServer.hpp"
+
 namespace server
 {
-    class PerfectServer {
-    public:
-        PerfectServer() {
+    PerfectServer::PerfectServer(int port) : port(port) {
 
+    }
+
+    PerfectServer::~PerfectServer() {
+        running = false;
+        stop();
+    }
+
+    void PerfectServer::setPort(int p) {
+        port = p;
+    }
+
+    // WS server goes here. ECS system goes
+
+    void PerfectServer::start() {
+
+        ws.onopen = [](const WebSocketChannelPtr& channel, const HttpRequestPtr& req) {
+            std::cout << "WebSocket client connected\n";
+        };
+        ws.onmessage = [](const WebSocketChannelPtr& channel, const std::string& msg) {
+            std::cout << "WS message: " << msg << "\n";
+        };
+        ws.onclose = [](const WebSocketChannelPtr& channel) {
+            std::cout << "WebSocket client disconnected\n";
+        };
+
+        ws_server = hv::WebSocketServer(&ws);
+        ws_server.setPort(port);
+        ws_thread = std::jthread(
+        [this](std::stop_token st){
+            async_ws_run(st);
         }
+        );
 
-        // WS server goes here. ECS system goes
-        private:
-        void stop() {
+        running = true;
+    }
 
-        }
-    };
-}
+    void PerfectServer::stop() {
+        ws_server.stop();
+    }
+
+    void PerfectServer::async_ws_run(std::stop_token st) {
+        ws_server.run();
+    }
+};
