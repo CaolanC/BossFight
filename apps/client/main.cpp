e76abe6f2f9abdef211fa141fd93d37579319342
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-03-28 06:32:34 +0000

    Removed client2 from main as client.run() blocks, code is never executed

--- apps/client/main.cpp
+++ apps/client/main.cpp
@@ -8,20 +8,12 @@ int main(int argc, char* argv[]) {
         auto client = client::Client(std::string("Perfect Client."), argv[1], false, true);
         if (client.start(argv[1])) {
             client.run(1920, 1080);
-            auto client2 = client::Client(std::string("Perfect Client."), argv[1], false, false);
-            if (client2.start(argv[1])) {
-                client2.run(1920, 1080);
-            }
         }
     }
     else {
         auto client = client::Client(std::string("Perfect Client."), "http://localhost:30000", false, true);
         if (client.start("http://localhost:30000")) {
             client.run(1920, 1080);
-            auto client2 = client::Client(std::string("Perfect Client."), argv[1], false, false);
-            if (client2.start("http://localhost:30000")) {
-                client2.run(1920, 1080);
-            }
         }
     }
     return 0;
