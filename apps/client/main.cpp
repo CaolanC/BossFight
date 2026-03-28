9b6689db122b78c1733e57716c336ceed7b08297
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-03-28 06:17:04 +0000

    Changed client initialization to reflect changes in client.cpp

--- apps/client/main.cpp
+++ apps/client/main.cpp
@@ -6,11 +6,23 @@ int main(int argc, char* argv[]) {
 
     if (argc == 2 ) {
         auto client = client::Client(std::string("Perfect Client."), argv[1], false, true);
-        client.run(1920, 1080);
+        if (client.start(argv[1])) {
+            client.run(1920, 1080);
+            auto client2 = client::Client(std::string("Perfect Client."), argv[1], false, false);
+            if (client2.start(argv[1])) {
+                client2.run(1920, 1080);
+            }
+        }
     }
     else {
-         auto client = client::Client(std::string("Perfect Client."), "http://localhost:30000", false, true);
-         client.run(1920, 1080);
+        auto client = client::Client(std::string("Perfect Client."), "http://localhost:30000", false, true);
+        if (client.start("http://localhost:30000")) {
+            client.run(1920, 1080);
+            auto client2 = client::Client(std::string("Perfect Client."), argv[1], false, false);
+            if (client2.start("http://localhost:30000")) {
+                client2.run(1920, 1080);
+            }
+        }
     }
     return 0;
 }
