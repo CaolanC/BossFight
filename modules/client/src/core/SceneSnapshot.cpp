47f4d8e7485acbd9ccdad21167a843a409098eac
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-03-31 06:05:25 +0100

    Added scene snapshot class

---
+++ modules/client/src/core/SceneSnapshot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ethan on 31/03/2026.
+//
+
+#include "SceneSnapshot.h"
---
+++ modules/client/src/core/SceneSnapshot.hpp
@@ -0,0 +1,13 @@
+//
+// Created by ethan on 31/03/2026.
+//
+
+#ifndef SOULJA_SCENESNAPSHOT_H
+#define SOULJA_SCENESNAPSHOT_H
+
+
+class SceneSnapshot {
+};
+
+
+#endif //SOULJA_SCENESNAPSHOT_H
