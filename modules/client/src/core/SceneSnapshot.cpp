13dad55e9dcb50aef8741a2d9ce060826cd3f44b
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-02 06:03:16 +0100

    Defined members and behaviour

--- modules/client/src/core/SceneSnapshot.cpp
+++ modules/client/src/core/SceneSnapshot.cpp
@@ -2,4 +2,26 @@
 // Created by ethan on 31/03/2026.
 //
 
-#include "SceneSnapshot.h"
+#include <core/SceneSnapshot.hpp>
+#include <core/SerializedObject.hpp>
+
+namespace core{
+    SceneSnapshot::SceneSnapshot() {
+    }
+
+    void SceneSnapshot::insert(const std::string& objectID, const SerializedObject& object) {
+        object_map[objectID] = object;
+    }
+
+    SerializedObject* SceneSnapshot::find(const std::string& object_id) {
+        auto it = object_map.find(object_id);
+        if (it == object_map.end()) {
+            return nullptr;
+        }
+        return &it->second;
+    }
+
+    std::unordered_map<std::string, SerializedObject> SceneSnapshot::getmap() {
+        return object_map;
+    }
+}
--- modules/client/src/core/SceneSnapshot.hpp
+++ modules/client/src/core/SceneSnapshot.hpp
@@ -2,12 +2,21 @@
 // Created by ethan on 31/03/2026.
 //
 
-#ifndef SOULJA_SCENESNAPSHOT_H
-#define SOULJA_SCENESNAPSHOT_H
+#include <unordered_map>
+#include <optional>
+#include <core/SerializedObject.hpp>
 
+namespace core {
+    class SceneSnapshot {
+    public:
+        SceneSnapshot();
 
-class SceneSnapshot {
-};
+        void insert(const std::string& objectID, const SerializedObject& object);
+        SerializedObject* find(const std::string& object_id);
+        std::unordered_map<std::string, SerializedObject> getmap();
 
+    private:
+        std::unordered_map<std::string, SerializedObject> object_map;
+    };
+}
 
-#endif //SOULJA_SCENESNAPSHOT_H
