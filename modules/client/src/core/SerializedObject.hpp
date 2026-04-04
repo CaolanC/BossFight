8ff8fbc5d8e71489441358cd2aa993c4213e49bf
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-04 07:26:09 +0100

    Added model_ref field

--- modules/client/src/core/SerializedObject.hpp
+++ modules/client/src/core/SerializedObject.hpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <glm/vec3.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include "crossguid/guid.hpp"
 
 namespace core
 {
     struct SerializedObject {
         std::string objectID;
-        std::string modelpath;
+        std::string model_path;
+        xg::Guid model_ref;
         glm::vec3 position;
         glm::quat rotation;
         float scale = 1.0f;
