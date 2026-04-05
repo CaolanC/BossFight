7beb20c0942c4845c6825ee63d67c714d0ff6ebe
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-05 06:17:10 +0100

    Added nlohmann json to project

---
+++ modules/client/src/core/SceneSerializer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ethan on 05/04/2026.
+//
+
+#include "SceneSerializer.hpp"
---
+++ modules/client/src/core/SceneSerializer.hpp
@@ -0,0 +1,13 @@
+//
+// Created by ethan on 05/04/2026.
+//
+
+#ifndef SOULJA_SCENESERIALIZER_HPP
+#define SOULJA_SCENESERIALIZER_HPP
+
+
+class SceneSerializer {
+};
+
+
+#endif //SOULJA_SCENESERIALIZER_HPP
