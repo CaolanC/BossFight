277085b3772d27249b781b4e9236b3e2dc84ccfc
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-06 06:35:28 +0100

    Added initialization from file - change init later to be blank or some other case

--- modules/client/src/systems/Init.cpp
+++ modules/client/src/systems/Init.cpp
@@ -49,4 +49,56 @@ namespace systems {
 
         return true;
     }
+
+    bool Init_from_file(entt::registry& r, const std::string& path, core::SceneSerializer& scene_serializer) {
+        // Same code as above but changed to work from a file
+        auto& model_m = r.ctx().get<component::model_manager>().manager;
+
+        std::vector<core::ShaderSource> shader_sources = {
+            core::sh_src::v3D(),
+            core::sh_src::fSolid()
+        };
+        ShaderProgramHandle program = r.ctx().get<component::material_manager>().manager.from_source_vec(shader_sources);
+
+        core::SceneSnapshot snapshot = core::SceneSnapshot();
+
+        bool ok = scene_serializer.load_from_file(path, snapshot);
+        snapshot.debug_print();
+        if (ok) {
+            for (const auto& [key, obj] : snapshot.getmap()) {
+                if (obj.model_path == "basicsphere") {
+                    par_shapes_mesh* par_m = par_shapes_create_subdivided_sphere(4);
+                    par_shapes_mesh* k_par_m = par_shapes_create_trefoil_knot(100, 100, 1);
+                    rendering::Model sphere_m = LoadModel(r, *par_m);
+                    auto const sphere_h = model_m.add_model(sphere_m, obj.model_ref);
+                    spawn::model(r, sphere_h, program, obj.model_path, obj.position, obj.scale, obj.rotation, obj.objectID);
+                }
+                else if (obj.model_path == "none") {
+                    rendering::Model model = rendering::Model();
+                    rendering::Node node = rendering::Node();
+                    std::vector<float> const v = {
+                        0.0f, 0.0f, 0.0f, 10.0f, 0.0f, 0.0f,
+                        0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 10.0f,
+                        10.0f, 0.0f, 10.0f, 10.0f, 0.0f, 0.0f,
+                        10.0f, 0.0f, 10.0f, 0.0f, 0.0f, 10.0f
+                    };
+                    node.mesh.add_primitive(rendering::pr_lines(v));
+                    model.root_nodes.push_back(node);
+                    ModelHandle const lh = model_m.add_model(model, obj.model_ref);
+                    spawn::model(r, lh, program, obj.model_path, obj.position, obj.scale, obj.rotation, obj.objectID);
+                }
+                else {
+                    rendering::Model m = LoadModel(r, obj.model_path);
+                    ModelHandle h = model_m.add_model(m, obj.model_ref);
+                    auto model_e = spawn::model(r, h, program, obj.model_path, obj.position, obj.scale, obj.rotation, obj.objectID);
+                    r.emplace<component::debug_spin>(model_e, 0.2f);
+                }
+
+            }
+            return true;
+        }
+
+        return false;
+    }
+
 }
--- modules/client/src/systems/Init.hpp
+++ modules/client/src/systems/Init.hpp
@@ -9,6 +9,11 @@
 
 #include "generator/GridPlane.hpp"
 
+#include <core/SceneSerializer.hpp>
+#include <core/SceneSnapshot.hpp>
+#include <core/SerializedObject.hpp>
+
 namespace systems {
     bool Init(entt::registry& r);
+    bool Init_from_file(entt::registry& r, const std::string& path, core::SceneSerializer& scene_serializer);
 }
