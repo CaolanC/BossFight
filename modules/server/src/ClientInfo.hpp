d6b56f6874909c268d7a4378266b84503f580289
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-10 07:33:06 +0100

    ClientInfo struct, Session stores a map with websocket channel and clientinfo struct

--- modules/server/src/ClientInfo.hpp
+++ modules/server/src/ClientInfo.hpp
@@ -2,6 +2,10 @@
 // Created by ethan on 10/04/2026.
 //
 
+#pragma once
+
+#include <crossguid/guid.hpp>
+
 struct ClientInfo {
     xg::Guid client_id;
     std::string role;
--- modules/server/src/Session.hpp
+++ modules/server/src/Session.hpp
@@ -2,6 +2,7 @@
 
 #include <crossguid/guid.hpp>
 #include <PerfectServer.hpp>
+#include <ClientInfo.hpp>
 
 namespace server
 
@@ -29,10 +30,14 @@ public:
     void startServer() { ps.start(); }
     void stopServer()  { ps.stop(); }
 
+    void addClient(const WebSocketChannelPtr& channel, ClientInfo client_info) {
+        connected_clients[channel] = client_info;
+    }
+
 private:
     xg::Guid id;
     PerfectServer ps;
-
+    std::unordered_map<WebSocketChannelPtr, ClientInfo> connected_clients;
 };
 
 }
