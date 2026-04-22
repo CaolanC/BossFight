91c10d53256d614795418f400e1eeb89c5a693ba
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-22 07:22:56 +0100

    LoadedModelInfo.hpp file, which helps with loaded model info data on the GUI

---
+++ modules/shared/src/LoadedModelInfo.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <string>
+#include <crossguid/guid.hpp>
+
+namespace core {
+    struct LoadedModelInfo {
+        xg::Guid model_ref;
+        std::string model_path;
+    };
+}
