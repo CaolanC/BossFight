304a8a879559140fe16fbecb269e37d7effd21e3
edoyle96 <ethan.doyle69@mail.dcu.ie>
2026-04-24 07:16:04 +0100

    Added checks for asset and save file existence

--- src/modules/client/src/utils/assets/helpers.cpp
+++ src/modules/client/src/utils/assets/helpers.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <utils/assets/helpers.hpp>
+#include <filesystem>
 
 namespace utils::assets {
     std::string get_asset(std::string const& path) {
@@ -12,4 +13,20 @@ namespace utils::assets {
         auto s = SLJA_SAVES_DIR + path;
         return s;
     };
+
+    bool asset_exists(std::string const& path) {
+        auto s = SLJA_ASSETS_DIR + path;
+        if (std::filesystem::exists(s)) {
+            return true;
+        }
+        return false;
+    }
+
+    bool filepath_exists(std::string const& path) {
+        auto s = SLJA_SAVES_DIR + path;
+        if (std::filesystem::exists(s)) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/modules/client/src/utils/assets/helpers.hpp
+++ src/modules/client/src/utils/assets/helpers.hpp
@@ -3,4 +3,6 @@
 namespace utils::assets {
     std::string get_asset(std::string const& path);
     std::string get_filepath(std::string const& path);
+    bool asset_exists(std::string const& path);
+    bool filepath_exists(std::string const& path);
 }
