1b4766b1f10796f0511ad4f6abed1a7b1cb38d53
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2026-09-07 18:23:08 +0100

    VIDEO: diffuse based on camera position

--- res/assets/shaders/fBasicLighting.glsl
+++ res/assets/shaders/fBasicLighting.glsl
@@ -36,7 +36,7 @@ layout(std140, binding = 1) uniform LightingUBO
 
 uniform sampler2D uTex;
 
-in vec3 vWorldPos;
+in vec3 FragPos;
 in vec3 vNorm;
 in vec2 vUv;
 
@@ -45,7 +45,7 @@ void main() {
     vec3 point_light = vec3(0);
 
     vec3 norm = normalize(vNorm);
-    vec3 light_dir = normalize(vNorm - vWorldPos);
+    vec3 light_dir = normalize(camera_position.xyz - FragPos);
 
     float diff = max(dot(norm, light_dir), 0.0);
     vec3 diffuse = diff * vec3(1.0f, 1.0f, 1.0f); // Custom light colors later
--- res/assets/shaders/v3D.glsl
+++ res/assets/shaders/v3D.glsl
@@ -12,13 +12,13 @@ layout (std140, binding=0) uniform CameraUBO {
 
 uniform mat4 uModel;
 
-out vec3 vWorldPos;
+out vec3 FragPos;
 out vec3 vNorm;
 out vec2 vuV;
 
 void main() {
     vec4 worldPos = uModel * vec4(aPos, 1.0);
-    vWorldPos = worldPos.xyz;
+    FragPos = worldPos.xyz;
 
     vNorm = mat3(uModel) * aNorm;
 
